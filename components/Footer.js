705207aa81ae55e726e2cefb5b6310feb48aed33
nicolas237 <nicolas@focomultimidia.com>
2023-06-30 18:55:07 -0300

    feat: alterando texto do about.me. Ajustes no pop-up de e-mail.

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends HTMLElement {
   }
   connectedCallback() {
     this.innerHTML = `
-    <footer class="footer is-100">
+    <footer class="footer is-100" style="margin-left: 2%">
       <div class="mail-me">
         <a href="mailto:souzanicolas87@gmail.com">
           <i class='bx bx-envelope'></i>
